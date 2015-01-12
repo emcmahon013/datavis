aa505402ff1cf2e85311375fd46244747031341d
ERIN MCMAHON <emcmahon013@gmail.com>
2015-01-11 20:18:19 -0500

    folder

--- assets/distday/week.js
+++ assets/distday/week.js
@@ -9,7 +9,7 @@ var margin = { top: 50, right: 0, bottom: 100, left: 30 },
           times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
 
 
-      d3.csv("/assets/distday/disthr.csv",
+      d3.csv("/datavis/assets/distday/disthr.csv",
         function(d) {
           return {
             day: +d.day,
